e5702d60f02b207a58960bd21d2b1aacd267637c
David Gutiérrez <dave.gutierrez0@gmail.com>
2024-10-16 18:57:52 -0500

    274. H-Index teaching me about generators

---
+++ 274. H-Index/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        num_papers = len(citations)
+
+        for h_index in range(max(citations),-1,-1):
+            
+            if h_index <= num_papers and h_index <= len([citation for citation in citations if citation >= h_index]):
+                return h_index     
+        
+        
+# Test case
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.hIndex(citations = [3,0,6,1,5]))    
+    print(solution.hIndex(citations = [1,3,1]))
+    
+# Time Complexity: O(n)
+# Space Complexity: O(1)
