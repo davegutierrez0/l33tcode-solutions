806b5955b0755d469b1dcf941945dd1a8567577c
David Gutiérrez <dave.gutierrez0@gmail.com>
2025-03-07 22:57:05 -0600

    improooving 88. Merged Sorted Array

--- 88. Merge Sorted Array/solution.py
+++ 88. Merge Sorted Array/solution.py
@@ -4,8 +4,21 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        for i, x in enumerate(nums1):
-            if i>m-1 and nums1[i] == 0:
-                nums1[i] = nums2.pop()
+        nums1_copy = nums1[0:m]
+        pointer_1 = 0 
+        pointer_2 = 0
+        write_pointer = 0
 
-        nums1.sort()
+        while write_pointer < len(nums1):
+            if pointer_2 >= len(nums2) or (pointer_1 < m and nums1_copy[pointer_1] <= nums2[pointer_2]):
+                nums1[write_pointer] = nums1_copy[pointer_1]
+                pointer_1 += 1
+
+            else:
+                nums1[write_pointer] = nums2[pointer_2]
+                pointer_2 += 1
+
+            write_pointer += 1
+
+# Time Complexity: (O(m + n))
+# Space Complexity: O(m)
