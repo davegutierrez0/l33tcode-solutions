2717b8a47fc6c855e97abdb652af9b656e80b63e
David Gutiérrez <dave.gutierrez0@gmail.com>
2025-03-16 22:05:12 -0500

    adding complexity

--- 48. Rotate Image/solution.py
+++ 48. Rotate Image/solution.py
@@ -1,56 +1,23 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix or not matrix[0]:
-            return []
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
 
-        m_rows = len(matrix)
-        n_cols = len(matrix[0])
-
-        num_elements = m_rows * n_cols
-        output = []
-
-        left_bound = 0
-        upper_bound = 0
-        right_bound = n_cols - 1
-        lower_bound = m_rows -1 
-
-
-
-        while left_bound <= right_bound and upper_bound <= lower_bound:
-
-            # Move Right
-            for x in range(left_bound, right_bound + 1):
-                output.append(matrix[upper_bound][x])
-            
-            upper_bound += 1
-
-
-            # Move Down
-            for y in range(upper_bound, lower_bound + 1):
-                output.append(matrix[y][right_bound])
-       
-            right_bound -= 1
-
-            # Move Left 
-            if upper_bound <= lower_bound:
-                for x in range(right_bound, left_bound - 1,-1):
-                    output.append(matrix[lower_bound][x])
-                
-                lower_bound -= 1
-
-
-
-            # Move Up   
-            if left_bound <= right_bound:
-                for y in range(lower_bound, upper_bound - 1, -1):
-                    output.append(matrix[y][left_bound])
-                
-                left_bound += 1
+        self.transpose(matrix)
+        self.reflect(matrix)
         
 
-            
+    def transpose(self,matrix: List[List[int]])-> None:
+        matrix[:] = map(list,zip(*matrix))
 
-        return output
+    def reflect(self, matrix: List[List[int]])-> None:
+        n = len(matrix)
 
-# Time Complexity: O(m*n)
-# Space Complexity: O(1)
+        for i in range(n): 
+            for j in range(n//2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+                
+# Time complexity: O(N^2)
+# Space complexity: O(N^2)
+```
