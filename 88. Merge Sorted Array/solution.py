3366fb78941fbba04cdf658ced7a5cc660e4fa07
David Gutierrez <dave.gutierrez@gmail.com>
2025-04-06 17:46:23 -0500

    more pythonic and no extra space

--- 88. Merge Sorted Array/solution.py
+++ 88. Merge Sorted Array/solution.py
@@ -4,21 +4,29 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        nums1_copy = nums1[0:m]
-        pointer_1 = 0 
-        pointer_2 = 0
-        write_pointer = 0
+        if not n:
+            # do nothing
+            pass
 
-        while write_pointer < len(nums1):
-            if pointer_2 >= len(nums2) or (pointer_1 < m and nums1_copy[pointer_1] <= nums2[pointer_2]):
-                nums1[write_pointer] = nums1_copy[pointer_1]
-                pointer_1 += 1
+        elif not m:
+            nums1[:] = nums2[:]
 
-            else:
-                nums1[write_pointer] = nums2[pointer_2]
-                pointer_2 += 1
+        else: 
+            # Start from back of each array
+            i, j = m - 1, n - 1
 
-            write_pointer += 1
+            for k in range(n + m - 1, -1, -1):
 
-# Time Complexity: (O(m + n))
-# Space Complexity: O(m)
+                if j < 0:
+                    break
+
+                if i >= 0 and nums1[i] > nums2[j]:
+                    nums1[k] = nums1[i]
+                    i -= 1
+                else:
+                    nums1[k] = nums2[j]
+                    j -= 1 
+
+
+# Time Complexity: O(m + n)
+# Space Complexity: O(1)
