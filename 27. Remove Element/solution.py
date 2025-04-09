0f71e9e01c207dc270104360d001eafb4cc04c7a
David Gutiérrez <dave.gutierrez0@gmail.com>
2025-04-08 22:54:34 -0500

    redoing merge intervals one-pass style

--- 27. Remove Element/solution.py
+++ 27. Remove Element/solution.py
@@ -1,11 +1,25 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0 
+        n = len(nums)
 
-        while val in nums:
-            nums.remove(val)
-        
-        nums.sort()
-        
-        return len(nums)
-    
-    # n^2 solution - come back to this 
+        while i < n:
+            if nums[i] == val:
+                # Replace current val with the last element
+                nums[i] = nums[n - 1]
+                # Decrease the size of "valid" array
+                n -= 1
+            else:
+                # Move to next index only if no removal
+                i += 1
+
+        return n
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
+# Swap with the last element if it matches.
+# Shrink the effective length of the array.
+# Only increment i when you don’t remove.
+# Don’t care about order, just care about who stays.
