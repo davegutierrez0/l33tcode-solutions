a60ea23f1154bf01594a273a09b4e0292e19135e
David Gutiérrez <dave.gutierrez0@gmail.com>
2025-04-10 21:29:19 -0500

    progress is progress

--- 35. Search Insert Position/solution.py
+++ 35. Search Insert Position/solution.py
@@ -1,50 +1,22 @@
-from typing import List
-
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
 
-        elif target < nums[0]:
-            return 0
+        left, right = 0, len(nums) - 1
 
-        elif target > nums[len(nums)-1]:
-            return len(nums)
+        while left <= right:
+            middle = (left + right) // 2
 
-        else: 
-            middleIndex = (len(nums)-1) // 2
-            # print("middleIndex:", middleIndex)
-
-
-            if nums[middleIndex -1] < target < nums[middleIndex]:
-                return middleIndex 
+            if nums[middle] == target:
+                return middle
             
-            elif target > nums[middleIndex]:
-                endIndex = len(nums) - 1
-                
-
-                while target < nums[endIndex]:
-                    endIndex -= 1
-                    
-                return endIndex + 1
+            elif nums[middle] < target:
+                left = middle + 1
             
-            elif target < nums[middleIndex]:
-                beginningIndex = 0
-                                                    
-                while target < nums[middleIndex]:
-                    middleIndex -= 1
+            else: 
+                right = middle - 1
 
+        return left # Left is the insertion position
 
-                return middleIndex + 1
-                
-                        
-
-# Test case
-if __name__ == "__main__":
-    solution = Solution()
-    print(solution.searchInsert(nums = [1,3,5,6], target = 5))
-    print(solution.searchInsert(nums = [1,3,5,6], target = 2))
-    print(solution.searchInsert(nums = [1,3,5,6], target = 7))
-    print(solution.searchInsert(nums = [1,3,5], target = 4))
-    
-    
+# Time Complexity: O(log N)
+# Space Complexity: O(1)
+# Mnemonic: Cut, Compare, Shift — “T > M? Then L jumps past M!”
