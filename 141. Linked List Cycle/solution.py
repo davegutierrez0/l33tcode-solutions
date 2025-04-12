d252fb30a86b801c42e386d80b6995994c29ae36
David Gutierrez <dave.gutierrez@gmail.com>
2025-04-12 15:31:13 -0500

    tortoise and hare with lower space complexity

--- 141. Linked List Cycle/solution.py
+++ 141. Linked List Cycle/solution.py
@@ -6,23 +6,26 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        node_set = set()
-        current = head
+        if not head:
+            return False
 
+        slow = head
+        fast = head.next
 
-        while current and current not in node_set:
-            node_set.add(current)
-            current = current.next
+        while fast != slow:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
 
-            if current and current in node_set:
-                return True
-
-        return False
 
 # Time Complexity: O(N)
-# Space Complexity: O(N)
+# Space Complexity: O(1)
 # N = number of nodes in the linked list
 
-# Approach: Hash Set
-# We traverse the list and store visited nodes in a set.
-# If we see the same node again, there is a cycle.
+# Approach: Floyd's Tortoise and Hare (Two Pointers)
+# Use a slow pointer and a fast pointer.
+# If there's a cycle, the fast pointer will eventually meet the slow pointer.
+# If there's no cycle, the fast pointer will reach the end of the list.
