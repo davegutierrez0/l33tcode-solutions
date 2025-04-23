7fc0099714e72fb84fc1cece738cb7d05532b2c7
David Gutiérrez <dave.gutierrez0@gmail.com>
2025-04-22 19:20:21 -0500

    adding a naive O(n) memory solution that has similar performance

--- 141. Linked List Cycle/solution.py
+++ 141. Linked List Cycle/solution.py
@@ -4,6 +4,42 @@
 #         self.val = x
 #         self.next = None
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        
+        seen = set()
+
+        seen.add(head)
+
+        current = head
+
+        while current.next:
+            if current.next not in seen:
+                seen.add(current.next)
+                current = current.next
+
+            else:
+                return True
+
+        return False
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+# N = number of nodes in the linked list
+# Approach: HashSet
+# Use a set to keep track of visited nodes.
+# If we encounter a node that is already in the set, there is a cycle.
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
