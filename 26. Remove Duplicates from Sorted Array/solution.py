7dceb12e4b9dc6007a1bb8818b447d537925c14b
David Gutiérrez <dave.gutierrez0@gmail.com>
2025-04-30 17:39:33 -0500

    more rreadable version

--- 26. Remove Duplicates from Sorted Array/solution.py
+++ 26. Remove Duplicates from Sorted Array/solution.py
@@ -15,3 +15,28 @@ class Solution:
 
 
         return i+1
+
+
+class Solution:
+def removeDuplicates(self, nums: List[int]) -> int:
+    if not nums:
+        return 0
+
+    write = 1
+
+    for read in range(1, len(nums)):
+        if nums[read] != nums[write-1]:
+            nums[write] = nums[read]
+            write +=1 
+
+    return write
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+# Mnemonic: “Two-Finger Scan”
+#   Two-Finger = two pointers (read and write).
+# 	Scan = you scan once through the list.
+
+# Picture yourself dragging one finger (“read”) along the sorted row of numbers, and tapping with the other
+# finger (“write”) only when you see a new unique. That one-pass, two-finger motion is O(N) time and uses 
+# just those two fingers—O(1) space.
